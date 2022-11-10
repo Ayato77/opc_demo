ea106c0a158173e0d9a02b661287a86e4b5970ae
Otoya Nakakaze <otoya.nakakaze@rwth-aachen.de>
2022-11-10 17:49:21 +0100

    Add files

---
+++ client.ts
@@ -0,0 +1,168 @@
+import {
+    OPCUAClient,
+    MessageSecurityMode,
+    SecurityPolicy,
+    AttributeIds,
+    makeBrowsePath,
+    ClientSubscription,
+    TimestampsToReturn,
+    MonitoringParametersOptions,
+    ReadValueIdOptions,
+    ClientMonitoredItem,
+    DataValue
+} from "node-opcua";
+
+const connectionStrategy = {
+    initialDelay: 1000,
+    maxRetry: 1
+}
+const options = {
+    applicationName: "MyClient",
+    connectionStrategy: connectionStrategy,
+    securityMode: MessageSecurityMode.None,
+    securityPolicy: SecurityPolicy.None,
+    endpointMustExist: false,
+};
+const client = OPCUAClient.create(options);
+// const endpointUrl = "opc.tcp://opcuademo.sterfive.com:26543";
+const endpointUrl = "opc.tcp://opcuaserver.com:48010";
+
+async function timeout(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main() {
+
+    /*const mqtt = require('mqtt')
+    const mqttClient  = mqtt.connect('mqtt://test.mosquitto.org')
+
+    mqttClient.on('connect', function () {
+        mqttClient.subscribe('presence', function (err) {
+            if (!err) {
+                mqttClient.publish('presence', 'Hello mqtt')
+            }
+        })
+    })
+
+    mqttClient.on('message', function (topic, message) {
+        // message is Buffer
+        console.log(message.toString())
+        mqttClient.end()
+    })*/
+
+    try {
+        // step 1 : connect to
+        await client.connect(endpointUrl);
+        console.log("connected !");
+
+        // step 2 : createSession
+        const session = await client.createSession();
+        console.log("session created !");
+
+        // step 3 : browsing root folder
+        const browseResult = await session.browse("RootFolder");
+
+        console.log("references of RootFolder :");
+        for(const reference of browseResult.references) {
+            console.log( "   -> ", reference.browseName.toString());
+        }
+
+        // step 4 : read a variable with readVariableValue
+        const dataValue2 = await session.read({
+            nodeId: "ns=1;s=free_memory",
+            attributeId: AttributeIds.Value
+        });
+        console.log(" value = ", dataValue2.toString());
+
+        // step 4' : read a variable with read
+        const maxAge = 0;
+        const nodeToRead = {
+            nodeId: 2258,
+            attributeId: AttributeIds.Value
+        };
+        const dataValue = await session.read(nodeToRead, maxAge);
+        console.log(" value ", dataValue.toString());
+
+        // step 5: install a subscription and install a monitored item for 10 seconds
+        const subscription = ClientSubscription.create(session, {
+            requestedPublishingInterval: 1000,
+            requestedLifetimeCount: 100,
+            requestedMaxKeepAliveCount: 10,
+            maxNotificationsPerPublish: 100,
+            publishingEnabled: true,
+            priority: 10
+        });
+
+        subscription
+            .on("started", function() {
+                console.log(
+                    "subscription started for 2 seconds - subscriptionId=",
+                    subscription.subscriptionId
+                );
+            })
+            .on("keepalive", function() {
+                console.log("keepalive");
+            })
+            .on("terminated", function() {
+                console.log("terminated");
+            });
+
+// install monitored item
+
+        const itemToMonitor: ReadValueIdOptions = {
+            nodeId: "ns=1;s=free_memory",
+            attributeId: AttributeIds.Value
+        };
+        const parameters: MonitoringParametersOptions = {
+            samplingInterval: 100,
+            discardOldest: true,
+            queueSize: 10
+        };
+
+        const monitoredItem = ClientMonitoredItem.create(
+            subscription,
+            itemToMonitor,
+            parameters,
+            TimestampsToReturn.Both
+        );
+
+        //TODO:check here the time and temperature of the oven
+        /*
+        * Depending on the temperature, decide if the baking time is appropriate.
+        * Then send the information via MQTT.
+        * This message should be received by a RasPi/ESP at the conveyor or vacuum lifter.
+        * The receiver sends a remove command if the corresponding product is defect
+        * */
+        monitoredItem.on("changed", (dataValue: DataValue) => {
+            console.log(" value has changed : ", dataValue.value.toString());
+        });
+
+        //TODO: Set subscription time to infinity
+        await timeout(10000);
+
+        //TODO: Terminate subscription only if there is an error
+        console.log("now terminating subscription");
+        await subscription.terminate();
+
+        // step 6: finding the nodeId of a node by Browse name
+        const browsePath = makeBrowsePath("RootFolder", "/Objects/Server.ServerStatus.BuildInfo.ProductName");
+
+        const result = await session.translateBrowsePath(browsePath);
+        const productNameNodeId = result.targets[0].targetId;
+        console.log(" Product Name nodeId = ", productNameNodeId.toString());
+
+        // close session
+        await session.close();
+
+        // disconnecting
+        await client.disconnect();
+        console.log("done !");
+    } catch(err) {
+        console.log("An error has occured : ",err);
+    }
+}
+
+
+main();
+
+
